4b2c81d8fc72f2ef0bb5f067bc2e207d1535a400
aFraley <humanimalmammal@webtv.net>
2016-05-30 16:18:30 -0500

    Fixed Celery Settings.

--- proj/proj/settings.py
+++ proj/proj/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # CELERY STUFF
 BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0re'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
